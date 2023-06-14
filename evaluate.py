bfbeea42bdd99d6fb3669c4e153aaab327cc8190
Meteor-xx <470411626@qq.com>
2023-06-14 15:40:24 +0800

    [ADD]bilstm+crf completed

---
+++ evaluate.py
@@ -0,0 +1,54 @@
+import time
+from collections import Counter
+from models.bilstm.bilstm_crf import BILSTM_Model
+from utils.utils import save_model, flatten_lists
+from utils.evaluating import Metrics
+
+
+def bilstm_train_and_eval(train_data, dev_data, test_data,
+                          word2id, tag2id, crf=True, remove_O=False):
+    train_word_lists, train_tag_lists = train_data
+    dev_word_lists, dev_tag_lists = dev_data
+    test_word_lists, test_tag_lists = test_data
+
+    start = time.time()
+    vocab_size = len(word2id)
+    out_size = len(tag2id)
+    bilstm_model = BILSTM_Model(vocab_size, out_size, crf=crf)
+    bilstm_model.train(train_word_lists, train_tag_lists,
+                       dev_word_lists, dev_tag_lists, word2id, tag2id)
+
+    model_name = "bilstm_crf" if crf else "bilstm"
+    save_model(bilstm_model, "./model_outs/"+model_name+".pkl")
+
+    print("训练完毕,共用时{}秒.".format(int(time.time()-start)))
+    print("评估{}模型中...".format(model_name))
+    pred_tag_lists, test_tag_lists = bilstm_model.test(
+        test_word_lists, test_tag_lists, word2id, tag2id)
+
+    print("*****Test_tag_lists[0]*****", test_tag_lists[0])
+    print("*****Pred_tag_lists[0]*****", pred_tag_lists[0])
+    metrics = Metrics(test_tag_lists, pred_tag_lists, remove_O=remove_O)
+    metrics.report_scores()
+    metrics.report_confusion_matrix()
+
+    return pred_tag_lists
+
+
+def ensemble_evaluate(results, targets, remove_O=False):
+    """ensemble多个模型"""
+    for i in range(len(results)):
+        results[i] = flatten_lists(results[i])
+
+    pred_tags = []
+    for result in zip(*results):
+        ensemble_tag = Counter(result).most_common(1)[0][0]
+        pred_tags.append(ensemble_tag)
+
+    targets = flatten_lists(targets)
+    assert len(pred_tags) == len(targets)
+
+    print("Ensemble 四个模型的结果如下：")
+    metrics = Metrics(targets, pred_tags, remove_O=remove_O)
+    metrics.report_scores()
+    metrics.report_confusion_matrix()
